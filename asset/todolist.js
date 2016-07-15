0d5edf7acf8d272f968b4d6a23456bb97a460834
Paul MC Kim <kmc5500@naver.com>
2016-07-15 17:22:31 +0900

    refactoring to TODO Obj

--- asset/todolist.js
+++ asset/todolist.js
@@ -1,8 +1,122 @@
+var TODO = {
+  ENTER_KEY_CODE : 13,
 
-//make sample test code
-var square = document.getElementById("test_div");
-var moveSquare = function(e)
-{
+  init : function(){
+    document.addEventListener("DOMContentLoaded", (function(){
+      document.addEventListener("keydown", this.add.bind(this));
+      document.addEventListener("click", this.click.bind(this));
+    }).bind(this));
+  },
+
+  add : function (e){
+    if(e.keyCode != this.ENTER_KEY_CODE){ return; }
+
+    var todoString = document.getElementById("new-todo").value;
+
+    if( todoString.trim() == ""){
+      return;
+    }
+    var li = this.make(todoString);
+    li.style.height = "0px";
+    var todoList = document.getElementById("todo-list");
+    todoList.appendChild(li);
+    this.slideDown(li, 500, 58);
+
+    document.getElementById("new-todo").value = "";
+  },
+
+  make : function(todoStr){
+    // //일반 TODO
+    // <li class="{}">
+    //   <div class="view">
+    //     <input class="toggle" type="checkbox" >
+    //     <label>타이틀</label>
+    //     <button class="destroy"></button>
+    //   </div>
+    // </li>
+
+    var input = document.createElement("input");
+    input.className = "toggle";
+    input.setAttribute("type", "checkbox");
+
+    var label = document.createElement("label");
+    label.innerHTML = todoStr;
+
+    var button = document.createElement("button");
+    button.className = "destroy";
+
+    var div_view = document.createElement("div");
+    div_view.className = "view";
+    div_view.appendChild(input);
+    div_view.appendChild(label);
+    div_view.appendChild(button);
+
+    var li = document.createElement("li");
+    li.appendChild(div_view);
+    return li;
+  },
+
+  click : function(e){
+    var findParentTodoLI = function (target){
+      var li = target;
+      while(li.tagName != "LI"){
+        li = li.parentNode;
+        if(!li) break;
+      }
+      return li;
+    };
+
+    var target = e.target;
+    // if checkbox click toggle completed
+    if(target.tagName ==="INPUT" && target.type == "checkbox"){
+      var li = findParentTodoLI(target);
+      li.classList.toggle("completed");
+    };
+
+    //if del button click del object
+    if(target.tagName === "BUTTON" && target.classList.contains("destroy") ){
+      var li = findParentTodoLI(target);
+      this.fadeout(li, 1000, function(element){
+        element.parentNode.removeChild(element);
+      });
+    };
+
+  },
+
+  fadeout : function(element, duration, callback){
+    var startTime = 0;
+    window.requestAnimationFrame(function animate(time){
+      if(startTime == 0) startTime = time;
+      var t = (time - startTime) / duration;
+      //console.log(1-t);
+      element.style.opacity = 1 - t;
+
+      if(time - startTime < duration ){
+        window.requestAnimationFrame(animate);
+      }
+      else {
+        callback(element);
+      }
+    });
+  },
+
+  slideDown : function (element, duration, height){
+    var startTime = 0;
+    window.requestAnimationFrame( function animate(time){
+      if(startTime == 0 ) startTime = time;
+      var t = (time - startTime)/duration;
+      element.style.height = "" + t*height+ "px";
+      //element.style.top = "" + (-(1-t)*height) + "px";
+      if( time - startTime < duration){
+        window.requestAnimationFrame(animate)
+      }
+    });
+  },
+};
+
+
+var moveSquare = function(e){ //sample test code for animation
+  var square = document.getElementById("test_div");
   var mouseX = e.clientX;
   var mouseY = e.clientY;
   var startX = square.offsetLeft;
@@ -10,7 +124,7 @@ var moveSquare = function(e)
 
   var startTime = 0;
   var aniTime = 1000;
-  requestAnimationFrame(function animate(time){
+  window.requestAnimationFrame(function animate(time){
     if(startTime === 0){
       startTime = time;
     }
@@ -18,120 +132,10 @@ var moveSquare = function(e)
     square.style.left = "" + (startX * (1-t) + mouseX * t) +"px" ;
     square.style.top =  "" + (startY * (1-t) + mouseY * t) +"px" ;
     if(time - startTime <= aniTime){
-       requestAnimationFrame(animate);
+       window.requestAnimationFrame(animate);
     }
   })
 }
 
-var fadeout = function(element, duration){
-  var startTime = 0;
-  requestAnimationFrame(function animate(time){
-    if(startTime == 0) startTime = time;
-    var t = (time - startTime) / duration;
-    console.log(1-t);
-    element.style.opacity = 1 - t;
-
-    if(time - startTime < duration ){
-      requestAnimationFrame(animate);
-    }
-    else {
-      element.parentNode.removeChild(element);
-    }
-  });
-}
-
-var mouseClick = function (e){
-  moveSquare(e); //test code
-  var target = e.target;
-
-  // if checkbox click
-  if(target.tagName ==="INPUT" && target.type == "checkbox"){
-    var li = findParentTodoLI(target);
-    li.classList.toggle("completed");
-  };
-
-  //if del button click
-  if(target.tagName === "BUTTON" && target.classList.contains("destroy") ){
-    var li = findParentTodoLI(target);
-    fadeout(li, 1000);
-  };
-
-
-}
-
-var makeTodo = function(todoStr){
-  // //일반 TODO
-  // <li class="{}">
-  //   <div class="view">
-  //     <input class="toggle" type="checkbox" >
-  //     <label>타이틀</label>
-  //     <button class="destroy"></button>
-  //   </div>
-  // </li>
-
-  var input = document.createElement("input");
-  input.className = "toggle";
-  input.setAttribute("type", "checkbox");
-
-  var label = document.createElement("label");
-  label.innerHTML = todoStr;
-
-  var button = document.createElement("button");
-  button.className = "destroy";
-
-  var div_view = document.createElement("div");
-  div_view.className = "view";
-  div_view.appendChild(input);
-  div_view.appendChild(label);
-  div_view.appendChild(button);
-
-  var li = document.createElement("li");
-  li.appendChild(div_view);
-  return li;
-
-}
-
-
-var findParentTodoLI = function (target){
-  var li = target;
-  while(li.tagName != "LI"){
-    li = li.parentNode;
-    if(!li) break;
-  }
-  return li;
-};
-
-var addTodo = function(e){
-  var ENTER = 13;
-  if(e.keyCode == ENTER)
-  {
-    var todoString = document.getElementById("new-todo").value;
-    if( todoString.trim() == ""){
-      return;
-    }
-    var li = makeTodo(todoString);
-    //li.style.top = "-24px";
-    li.style.height = "0px";
-    var todoList = document.getElementById("todo-list");
-    todoList.appendChild(li);
-    slideDown(li, 500, 58);
-
-    document.getElementById("new-todo").value = "";
-  }
-}
-
-var slideDown = function (element, duration, height){
-  var startTime = 0;
-  requestAnimationFrame( function animate(time){
-    if(startTime == 0 ) startTime = time;
-    var t = (time - startTime)/duration;
-    element.style.height = "" + t*height+ "px";
-    //element.style.top = "" + (-(1-t)*height) + "px";
-    if( time - startTime < duration){
-      requestAnimationFrame(animate)
-    }
-  });
-}
-
-document.addEventListener("click", mouseClick);
-document.addEventListener("keydown", addTodo);
+document.addEventListener("click", moveSquare);
+TODO.init();
